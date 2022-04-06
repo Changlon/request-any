7e6adf7e2defd3960a2b8688fe001a56685f8307
Changlon <changlong.a2@gmail.com>
2022-04-06 16:54:11 +0800

    types

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+type reqAny = (route?:string,data?:{[k:string]:any},type?:string,json?:boolean,baseUrl?:string,timeout?:number,headers?:{[k:string]:any})=> Promise<any> 
+export default function generateReqAny(reqUrl?:string):reqAny 
+
+
+
+
+
+
+ 
+
+
+ 
